57711a4525884a31b8a1cc1325f77c46f6296506
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-05-09 21:13:00 -0400

    added __contains__ method and tests

--- Week 1/in_your_interface.py
+++ Week 1/in_your_interface.py
@@ -1,12 +1,27 @@
 class Teams:
-  def __init__(self, members):
-    self.__myTeam = members
+    def __init__(self, members):
+        self.__myTeam = members
 
-  def __len__(self):
-    return len(self.__myTeam)
+    def __len__(self):
+        return len(self.__myTeam)
+
+    def __contains__(self, x):
+        """Method to return membership test"""
+
+        if x in self.__myTeam:
+            return True
+        return False
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        
 
 def main():
-  classmates = Teams(['John', 'Steve', 'Tim'])
-  print (len(classmates))
+    classmates = Teams(['John', 'Steve', 'Tim'])
+    print(len(classmates))
+    print('Tim' in classmates) # Test __contains__
+    print('Bob' in classmates) # Test __contains__
 
 main()
