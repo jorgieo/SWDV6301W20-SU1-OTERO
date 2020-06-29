7e4ac2cc20c2c34247fd1935abb46185d27248e4
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-06-28 22:46:41 -0400

    removed references to description dictionary.

--- Week 8/returned.py
+++ Week 8/returned.py
@@ -15,22 +15,23 @@ class Return(Transaction, Base):
 
     def returnItems(self, *items):
         for item in items:
-            if item in self.sale_object.description['items']:
+            if item in self.sale_object.items:
                 self.returns.append(item)
             else:
                 print(f"Item '{item}' not found! Check the entry and try again.")
         return self.returns
 
-    def storeCredit(self):
-        credit = 0
-        for item in self.returns:
-            if item in self.inventory.getInventory().keys():
-                credit += self.inventory.getInventory()[item]
-        return credit
+    # def storeCredit(self):
+    #     credit = 0
+    #     for item in self.returns:
+    #         if item in self.inventory.getInventory().keys():
+    #             credit += self.inventory.getInventory()[item]
+    #     return credit
 
     __tablename__ = "transactions"
 
     id = Column(Integer, primary_key=True)
     timestamp = Column(String)
     transID = Column(String)
-    description = Column(String)
+    type = Column(String)
+    netTotal = Column(Numeric)
